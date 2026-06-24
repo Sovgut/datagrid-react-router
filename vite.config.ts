a30fe28f55549405c897c3318c08aa4332fbba6a
Serhii Sovhut <sergey.sovgut@outlook.com>
2026-06-24 17:25:47 +0300

    fix: fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       fileName: "datagrid-react-router",
       formats: ["es"],
     },
-    rollupOptions: {
+    rolldownOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
     },
   },
